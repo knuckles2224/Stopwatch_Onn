2488280074d2e6a0777e5db215bb8be69f2b7a65
Rathanank Onn <tuk86465@temple.edu>
2020-02-21 01:53:39 -0500

    Initial Commit

---
+++ Stopwatch_Onn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stopwatch_Onn
+//
+//  Created by Anthony Onn on 2/21/20.
+//  Copyright © 2020 Anthony Onn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
