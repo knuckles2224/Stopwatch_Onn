f4f0b2a3646681adc0557f3b329781e9876ef6a5
Rathanank Onn <tuk86465@temple.edu>
2020-02-23 04:11:03 -0500

    init commit

---
+++ Stopwatch_Onn/Stopwatch.swift
@@ -0,0 +1,51 @@
+//
+//  Stopwatch.swift
+//  Stopwatch_Onn
+//
+//  Created by Anthony Onn on 2/21/20.
+//  Copyright © 2020 Anthony Onn. All rights reserved.
+//
+
+import Foundation
+
+// Goals: find current time, calculate elapsed time, and convert elapsed time to string
+class Stopwatch {
+    
+    private var startTime: NSDate?
+    
+    var isRunning: Bool?
+    
+    var time: Int = 0
+    
+    var elapsedTime: TimeInterval {
+        if let startTime = self.startTime {
+            return -1 * startTime.timeIntervalSinceNow
+        } else {
+            return 0
+        }// else return 0
+    }//end var
+    
+    var elapsedTimeAsString: String {
+     
+        time = time + 1
+        
+        let seconds:Int = (time / 10) % 60  // 1 second pass, divide by 10
+        let minutes:Int = (time / 600) % 60 // 60 seconds pass, divide by 10 * 60
+        let milliseconds:Int = (time) % 10
+        return String(format: "%02d:%02d:%01d" , minutes ,seconds, milliseconds)
+        //return String(format: "%02.f:%02.f:%1.1f" , elapsedTime.truncatingRemainder(dividingBy: 59),
+                      //elapsedTime.truncatingRemainder(dividingBy: 59), elapsedTime) //<- this converts time elapsed to string
+        //need variables for minutes, seconds , milliseconds
+
+       // return(String)(milliseconds)
+    }//end elapsed time as string
+    
+    func start() {
+        startTime = NSDate()
+    }//end start
+    
+    func stop() {
+        startTime = nil
+    }//end stop
+    
+}//end class Stopwatch
--- Stopwatch_Onn/ViewController.swift
+++ Stopwatch_Onn/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let stopwatch = Stopwatch() //create instance of Stopwatch
+    
+    //code to stop the clock
+    @IBAction func startButtonTapped(_ sender: UIButton) {
 
+        //checks if stopwatch is not running <- if true, run. Otherwise it is already running
+        if(stopwatch.isRunning != true) {
+            stopwatch.isRunning = true
+            Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.updateElapsedTimeLabel), userInfo: self, repeats: true)
+            stopwatch.start()
+        }//end stopwatch is not running doing this
+    }//end startButtonTapped
+    
+    //code to stop the clock
+    @IBAction func stopButtonTapped(_ sender: UIButton) {
+
+        //if stopwatch is not running and stop is clicked, reset time.
+        if(stopwatch.isRunning == false) {
+            timeElapsed.text = "00:00:0"
+            stopwatch.time = 0
+        }//if watch is already stopped
+        
+        //set isRunning to false and call stop func
+        stopwatch.isRunning = false
+        stopwatch.stop()
+    }//end stopButtonTapped
+    
+    @IBOutlet weak var timeElapsed: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+    }//end viewdidload
 
 
-}
+    @objc func updateElapsedTimeLabel(timer: Timer) {
+       
+        if (stopwatch.isRunning == true) {
+            timeElapsed.text = stopwatch.elapsedTimeAsString
+        } else {
+            timer.invalidate()
+        }
+    }//end func updateElapsedTimeLabel
+    
+}//end class
 
